4a39590a5495666db930fabbad65bc7cd4f529b1
Samson Ssali <ssali@jitpomi.com>
2024-05-21 01:21:52 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# magnifying-glass
-Created with CodeSandbox
+# vue-inline-zoom
+An inline zoom component
+# Stack
+- Native Javascript Dom Object
+- VueJS 
+- Css
+# Code Sandbox
+https://codesandbox.io/embed/github/jermsam/vue-inline-zoom/tree/main/?fontsize=14&hidenavigation=1&theme=dark 
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="box">
+    <div style="width: 80vw">
+      <j-vue-inline-magnifier
+        :src="img"
+        style="max-height: 100%; cursor: grab"
+        width="210px"
+        height="140px"
+        :zoom-factor="1.5"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import JVueInlineMagnifier from "./components/JVueInlineMagnifier";
+
+export default {
+  name: "App",
+  components: { JVueInlineMagnifier },
+  data() {
+    return {
+      img:
+        "https://i.etsystatic.com/6448977/r/il/6da6eb/1038951011/il_1588xN.1038951011_gajr.jpg",
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background: #E2EEFC;
+  margin: 0;
+}
+</style>
